b1353b7c5428328d71d8d1bfc420a91b79ae8535
Beatriz Jorge <beatriz.jorgeb@viewnext.com>
2024-02-28 18:25:15 +0100

    feat(estructura completa): :art:

--- Proyectos/catalogue/src/app/app.module.ts
+++ Proyectos/catalogue/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductComponent } from './catalogue/components/product/product.component';
+import { FilterComponent } from './catalogue/components/filter/filter.component';
+import { HomeComponent } from './catalogue/page/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    FilterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ Proyectos/catalogue/src/app/catalogue/components/filter/filter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'catalogue-filter',
+  templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.scss']
+})
+export class FilterComponent {
+
+}
